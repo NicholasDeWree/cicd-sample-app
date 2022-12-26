55fecb53f2cf3e7e1ecfc10157a58785eabff64f
Nicholas De Wree <47564418+NicholasDeWree@users.noreply.github.com>
2022-12-26 14:38:01 +0100

    Update sample_app.sh

--- sample_app.sh
+++ sample_app.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir tempdirectorystr
-mkdir tempdirectorystr/templates
-mkdir tempdirectorystr/static
+mkdir tempdirectorystra
+mkdir tempdirectorystra/templates
+mkdir tempdirectorystra/static
 
-cp sample_app.py tempdirectorystr/.
-cp -r templates/* tempdirectorystr/templates/.
-cp -r static/* tempdirectorystr/static/.
+cp sample_app.py tempdirectorystra/.
+cp -r templates/* tempdirectorystra/templates/.
+cp -r static/* tempdirectorystra/static/.
 
-cat > tempdirectorystr/Dockerfile << _EOF_
+cat > tempdirectorystra/Dockerfile << _EOF_
 FROM python
 RUN pip install flask
 COPY  ./static /home/myapp/static/
@@ -19,7 +19,7 @@ EXPOSE 5050
 CMD python /home/myapp/sample_app.py
 _EOF_
 
-cd tempdirectorystr || exit
+cd tempdirectorystra || exit
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a 
